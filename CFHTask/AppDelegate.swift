480bf4d35b0ea9be9df9b6b35f97b6b0743b3c3f
FoxLi <fox@mitake.com.tw>
2021-10-04 09:11:55 +0800

    for update test

--- CFHTask/AppDelegate.swift
+++ CFHTask/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if #available(iOS 13.0, *) {
             self.window?.overrideUserInterfaceStyle = .light
         }
+        
         return true
     }
 }
